f7f3a1417a7c339b8fa5bf4ebea86f1041d73db3
sdfsffes <banblossom100@gmail.com>
2026-06-22 16:21:04 +0300

    Fix: disable CSS minification and clean configs

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,21 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   css: {
-    minify: false
+    devSourcemap: false,
+    postcss: {
+      plugins: []
+    }
   },
   build: {
     cssMinify: false,
+    minify: false,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]'
+      }
+    }
+  },
+  esbuild: {
     minify: false
   }
 })
