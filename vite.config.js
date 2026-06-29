f02565127f237e192beeaf29fe4dc6fcc88c320a
sdfsffes <banblossom100@gmail.com>
2026-06-29 16:14:13 +0300

    Fix: Vercel build configuration

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,16 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    minify: false,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  },
   css: {
-    postcss: './postcss.config.js'
+    minify: false
   }
 })
